3ee9f371e578a8b05240069c3d731f74b1bd89a2
Sarah Childs <sarahchilds19@gmail.com>
2014-03-04 16:35:47 -0500

    no message

---
+++ Unity Implementation/Assets/Character.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    public Vector2 size;
+    public Vector2 position;
+    private float speed;
+
+    public Texture2D sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = Resources.Load<Texture2D>("sprite") as Texture2D;
+        position = transform.position;
+        size = new Vector2(sprite.width, sprite.height);
+        speed = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position.x = 0;
+
+        transform.Translate(position.x + speed * Time.deltaTime, position.y * Time.deltaTime, 0);
+	}
+}
---
+++ Unity Implementation/Assets/Collider_Demo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collider_Demo : MonoBehaviour {
+
+    public enum MaterialType { 
+        Regular, Slippery, Sticky, Bouncy
+    }
+
+    public Vector2 size;
+    public Vector2 center;
+    public MaterialType material;
+
+    public Character sprite;
+
+	// Use this for initialization
+	void Start () {
+        size = sprite.size;
+        center = Vector2.zero;
+        material = MaterialType.Regular;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
